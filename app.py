f0a0b36555a12315dc1fa8b473cc99451ef7bae9
a33publicores <a33publicores@gmail.com>
2026-05-12 21:31:25 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify, request
+
+app = Flask(_name_)
+
+@app.route("/")
+def home():
+    return "PlayBar Go Player API"
+
+@app.route("/player/status")
+def player_status():
+    cliente = request.args.get("cliente", "A33")
+
+    return jsonify({
+        "ok": True,
+        "actual": {
+            "videoId": "demo1",
+            "titulo": "Video de prueba",
+            "video_url": "https://www.w3schools.com/html/mov_bbb.mp4"
+        },
+        "siguiente": {
+            "titulo": "Próximo video"
+        }
+    })
+
+@app.route("/player/ended", methods=["POST"])
+def player_ended():
+    data = request.json
+    cliente = data.get("cliente")
+    print(f"Video finalizado para {cliente}")
+
+    return jsonify({"ok": True})
+
+if _name_ == "_main_":
+    app.run(host="0.0.0.0", port=5000)
