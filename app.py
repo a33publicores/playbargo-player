76faa6700e384e8532bc92fb8b16c8882ee20a5e
a33publicores <a33publicores@gmail.com>
2026-05-13 13:43:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def obtener_video_url(video_id):
         youtube_url = f"https://www.youtube.com/watch?v={video_id}"
 
         ydl_opts = {
-            "format": "best[protocol!=dash][acodec!=none][height<=1080]/best[acodec!=none]/best",
+            "format": "best[protocol!=dash][acodec!=none][height>=1080]/best[acodec!=none]/best",
                 "quiet": True,
                 "noplaylist": True,
                 "nocheckcertificate": True,
